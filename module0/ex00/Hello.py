a8deb68b8b72b629490ae411f76de0f7e9f5abea
LuckyIntegral <frantsv2004@gmail.com>
2024-06-13 19:34:40 +0200

    add ex00/module0

---
+++ module0/ex00/Hello.py
@@ -0,0 +1,18 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list[1] = 'World!'                  # access elements with [] operator
+
+ft_tuple = (ft_tuple[0], 'Austria!')   # immutable, so create a new tuple
+
+ft_set.remove('tutu!')                 # remove the element tutu! from the set
+ft_set.add('Vienna!')                  # add required element to the set
+
+ft_dict["Hello"] = '42Vienna!'         # in map, access elements by key
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
